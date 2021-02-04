e67e84692d4882a043fe58a98f042d91e2041882
NathanReynoso <sourlemonz111@gmail.com>
2021-02-04 03:20:41 +0000

    Added tests for calc distance and calc shortest path

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -13,3 +13,19 @@ print("testing dfs...")
 df = graph.get_nodes_depth_first(2)
 assert [node.index for node in df] == [2,1,4,5,3,0]
 print("Passed")
+
+print("testing calc_distance")
+assert graph.calc_distance(0,4) == 2
+assert graph.calc_distance(5,2) == 3
+assert graph.calc_distance(0,5) == 3
+assert graph.calc_distance(4,1) == 1
+assert graph.calc_distance(3,3) == 0
+print("Passed")
+
+print("testing calc_shortest_path")
+assert graph.calc_shortest_path(0,4) == [0,1,4]
+assert graph.calc_shortest_path(5,2) == [5,4,1,2]
+assert graph.calc_shortest_path(0,5) == [0,1,4,5]
+assert graph.calc_shortest_path(4,1) == [4,1]
+assert graph.calc_shortest_path(3,3) == [3]
+print("Passed")
